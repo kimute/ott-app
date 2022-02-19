7f8f24869c723b1d35ee32c5c8bb545be6bf6953
kimute <jkdnekki@icloud.com>
2022-02-19 16:33:38 +0900

    add intro page

---
+++ README.md
@@ -0,0 +1,26 @@
+# Over The Top service project
+
+Movie & Tv show Trailer Streaming service
+
+Description:
+
+- Intro page
+- Move
+- Tv Show
+- Add Mylist
+- Search
+
+### Stack:
+
+<img alt="React" src ="https://img.shields.io/badge/React-v17.0.2-61DAFB.svg?&style=for-the-badge&logo=React&logoColor=61DAFB"/>
+<img alt="TypeScript" src ="https://img.shields.io/badge/TypeScript-v4.5.5-3178C6.svg?&style=for-the-badge&logo=TypeScript&logoColor=3178C6"/>
+<img alt="Styled Components" src ="https://img.shields.io/badge/Styled_Components-5.3.3-DB7093.svg?&style=for-the-badge&logo=StyledComponents&logoColor=DB7093"/>
+
+### Using:
+
+- <img alt="Recoil" src ="https://img.shields.io/badge/Recoil-0.6.1-0078D4.svg?&style=for-the-badge&logo=Recoil&logoColor=0078D4"/>
+- <img alt="ReactQuery" src ="https://img.shields.io/badge/React_Query-3.34.14-FF4154.svg?&style=for-the-badge&logo=ReactQuery&logoColor=FF4154"/>
+- <img alt="React Router DOM" src ="https://img.shields.io/badge/React_Router_DOM-5.3.0-CA4245.svg?&style=for-the-badge&logo=ReactRouterDOM&logoColor=CA4245"/>
+- <img alt="React hook form" src ="https://img.shields.io/badge/React_Hook_form-5.3.0-4285F4.svg?&style=for-the-badge&logo=ReactHookForm&logoColor=4285F4"/>
+- <img alt="React Player" src ="https://img.shields.io/badge/React_Player-5.3.0-000000.svg?&style=for-the-badge&logo=ReactPlayer&logoColor=000000"/>
+- <img alt="Framer Motion" src ="https://img.shields.io/badge/Framer_Motion-6.2.4-0055FF.svg?&style=for-the-badge&logo=FramerMotion&logoColor=0055FF"/>
--- src/Routes/Intro.tsx
+++ src/Routes/Intro.tsx
@@ -1,16 +1,16 @@
-import { Link, useRouteMatch } from "react-router-dom";
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import { useHistory} from "react-router-dom";
 import styled from "styled-components";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
     height: 100vh;
     width: 100%;
-    min-width: 630px; 
     display: flex;
     justify-content: center;
     align-items: center;
 `;
 
-const Roll = styled.div`
+const Roll = styled(motion.div)`
     display: flex;
     justify-content: center;
     align-items: center;
@@ -23,24 +23,50 @@ const Roll = styled.div`
     border-radius: 50px;
     border: 2px solid ${props => props.theme.blue};
     transition: 0.2s linear;
+    cursor: pointer;
     &:hover{
         background-color: ${props => props.theme.blue};
         color: black;
         border:none;
     }
 `;
-
+const Title = styled.h3`
+    font-size: 30px;
+`;
+const SecondTitle = styled.h3`
+    margin-top: 10px;
+    margin-left: 15px;
+    font-size: 17px;
+`;
 
 function Intro(){
+    const x = useMotionValue(0);
+    const history = useHistory();
+    const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+    const goHomeClick = () => history.push("/home");
+    const gradient = useTransform(
+    x, [-800,0 ,800], 
+    [
+      'linear-gradient(135deg, rgb(0, 210 ,248), rgb(0, 83 ,238))',
+      'linear-gradient(135deg, rgb(0, 183 ,153), rgb(0, 210 ,238))',
+      'linear-gradient(135deg, rgb(0, 238 ,155), rgb(238, 178 ,0))',
+    ])
+  
     return(
         <>
-        <Wrapper>
-        <Link to="/home">
-            <Roll>
-                <span>Movie &</span>
-                <span>more</span>
+        <Wrapper style={{background: gradient}}>
+        <Roll 
+              onClick={goHomeClick }
+              style={{x, rotateZ}} 
+              initial={{x: -800}} 
+              animate={{ x:0, transition: {duration: 1.5}}}
+              drag="x" dragSnapToOrigin>
+         
+                <Title>Movie</Title>
+                <SecondTitle>&More..?</SecondTitle>
+            
+              
             </Roll>
-        </Link>
         </Wrapper>
         </>
         
